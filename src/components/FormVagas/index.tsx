c1b563615e4c89c433903601c4a3cb3730b2e261
Leo Coutinho <leocoutinho194@gmail.com>
2024-08-15 09:49:44 -0300

    corrigindo problemas

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -2,7 +2,7 @@ import { FormEvent, useState } from 'react'
 import { FormVagas as Formulary } from './styles'
 import { Campo } from './styles'
 import { BtnPesquisar } from './styles'
-import styles from './FormVagas.module.css'
+// import styles from './FormVagas.module.css'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -19,7 +19,6 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   return (
     <Formulary onSubmit={aoEnviarForm}>
       <Campo
-        className={styles.campo}
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
